a5e1d89f49ee8a2b05fb6477dd06fc9bafbad78e
ddaniel-bit <67413475+ddaniel-bit@users.noreply.github.com>
2020-06-25 11:20:04 +0200

    Small updates

--- main.ts
+++ main.ts
@@ -77,4 +77,5 @@ titkositott_szoveg = ""
 character = 0
 abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "!", ".", "?"]
 secret = ["*", "|", "9", "Ł", "ß", "7", "1", "]", "%", "+", "/", "w", "\\", "[", "€", "o", "÷", "i", "{", "a", "f", "}", "Đ", "$", "&", "j", "#", ">", "6", "<"]
+basic.showString("by:ddaniel-bit")
 basic.showString("" + (abc[character]))
